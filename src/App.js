1960b3a0fa79eeed59ba0cd73c4baa9acf628a4c
HariharanG <harigopallak45@gmail.com>
2025-07-09 18:04:19 +0530

    Commit to show thhe conflict

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Edit <code>src/App.js</code> and Commit every time once you complete a task
+        Edit <code>src/App.js</code> and Commit conflict on same file
         </p>
         <a
           className="App-link"
